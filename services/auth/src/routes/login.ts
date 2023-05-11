1a4fc1c994768c80fdaa137310d4af12f731198d
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-05-11 13:58:52 -0400

    feat: auth add validation for auth-login route

--- services/auth/src/routes/login.ts
+++ services/auth/src/routes/login.ts
@@ -1,14 +1,26 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.route("/users/login").get((req, res, next) => {
-  const { email, password } = req.body;
-  console.log("req login");
+router
+  .route("/users/login")
+  .get(
+    body("email").isEmail().withMessage("Please use valid email."),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Please use valid password length (min: 4, max: 20)."),
+    (req: Request, res: Response, next) => {
+      const { email, password } = req.body;
+      console.log("req login");
 
-  res.status(200).send("Req login OK");
+      res
+        .status(200)
+        .send(`Req login OK: {email: ${email}, password: ${password}}`);
 
-  next();
-});
+      next();
+    }
+  );
 
 export { router as routerLogin };
