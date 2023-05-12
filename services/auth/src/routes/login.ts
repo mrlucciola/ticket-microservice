34435b650f36eee313dc0476470109f0a5ab7299
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-05-11 20:00:21 -0400

    auth move login code to register

--- services/auth/src/routes/login.ts
+++ services/auth/src/routes/login.ts
@@ -1,26 +1,9 @@
-import { Request, Response, Router } from "express";
-import { body } from "express-validator";
+import { Router } from "express";
 
 const router = Router();
 
-router
-  .route("/users/login")
-  .get(
-    body("email").isEmail().withMessage("Please use valid email."),
-    body("password")
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage("Please use valid password length (min: 4, max: 20)."),
-    (req: Request, res: Response, next) => {
-      const { email, password } = req.body;
-      console.log("req login");
-
-      res
-        .status(200)
-        .send(`Req login OK: {email: ${email}, password: ${password}}`);
-
-      next();
-    }
-  );
+router.route("/users/login").post((_req, res, _next) => {
+  res.status(200).send(`Req login OK`);
+});
 
 export { router as routerLogin };
