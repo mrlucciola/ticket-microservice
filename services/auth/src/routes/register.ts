29b51ca7dfd699dbd7bd54937a94ba53e37cfaa4
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-05-11 20:00:21 -0400

    refactor: auth move login code to register

--- services/auth/src/routes/register.ts
+++ services/auth/src/routes/register.ts
@@ -1,13 +1,29 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
-router.route("/users/register").get((_req, res, next) => {
-  console.log("req register user");
+router
+  .route("/users/register")
+  .post(
+    body("email").isEmail().withMessage("Please use valid email."),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Please use valid password length (min: 4, max: 20)."),
+    (req: Request, res: Response, _next) => {
+      // validate input
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).send(errors.array());
+      } else {
+        const { email, password } = req.body;
 
-  res.status(200).send("Req to register OK");
-
-  next();
-});
+        res
+          .status(200)
+          .send(`Req register OK: {email: ${email}, password: ${password}}`);
+      }
+    }
+  );
 
 export { router as routerRegister };
