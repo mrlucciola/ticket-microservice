25e5d3bd9abf8e6ad50c98ac5dbb1f696bdb0bb9
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-05-16 12:18:02 -0400

    refactor: auth error handling logical grouping
    needs to be done so errors are specified in their respective files

--- services/auth/src/errors/databaseConnectionError.ts
+++ services/auth/src/errors/databaseConnectionError.ts
@@ -1,9 +1,16 @@
+import { Response } from "express";
 import { EventError } from ".";
 
 export class DatabaseConnectionError extends EventError {
   reason = "Error connecting to database";
+  status: number = 500;
 
-  constructor() {
-    super();
+  constructor(res: Response) {
+    super(res);
+
+    this.message = this.reason;
+
+    // for extending builtin
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 }
