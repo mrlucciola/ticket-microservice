fc229bcd999b8e49aadbe8d2c9c3e83d7dabb64b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-06-08 14:43:33 -0400

    (13) auth- add requireAuth middleware and UnauthorizedError

--- services/auth/src/routes/currentUser.ts
+++ services/auth/src/routes/currentUser.ts
@@ -1,10 +1,16 @@
 import { Request, Response, Router } from "express";
 import { currentUser } from "../middlewares/currentUser";
+import { requireAuth } from "../middlewares/requireAuth";
 
 const router = Router();
 
-router.get("/users/currentuser", currentUser, (req: Request, res: Response) => {
-  res.status(200).send({ currentUser: req.currentUser || null });
-});
+router.get(
+  "/users/currentuser",
+  currentUser,
+  requireAuth,
+  (req: Request, res: Response) => {
+    res.status(200).send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as routerCurrentUser };
