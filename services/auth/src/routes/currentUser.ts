4c3394354e3b0e9020bb9b2184c2499f2f78a185
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2023-06-07 20:33:13 -0400

    9 auth implement current user logic (#10)
    * (9) auth- currentuser- add jwt token exists check
    
    * (9) auth- currentuser- add jwt validation logic

--- services/auth/src/routes/currentUser.ts
+++ services/auth/src/routes/currentUser.ts
@@ -1,11 +1,23 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.route("/users/currentuser").get((_req, res, next) => {
-  console.log("req current user");
+router.route("/users/currentuser").get((req: Request, res: Response, next) => {
+  const jwtSessionToken = req.session?.jwt;
+  if (jwtSessionToken) {
+    // handle error: check if jwt exists
+    res.send({ currentUser: null });
+  } else {
+    // handle error: check if jwt is valid
+    try {
+      const payload = jwt.verify(jwtSessionToken, process.env.JWT_KEY!);
 
-  res.status(200).send("Req to currentuser OK");
+      res.status(200).send({ currentUser: payload });
+    } catch (err) {
+      res.send({ currentUser: null });
+    }
+  }
 
   next();
 });
