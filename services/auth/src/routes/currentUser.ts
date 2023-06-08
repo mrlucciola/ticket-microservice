e82ade17be0055262d38f32b0c4733c4f428d133
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-06-07 20:22:20 -0400

    (9) auth- currentuser- add jwt token exists check

--- services/auth/src/routes/currentUser.ts
+++ services/auth/src/routes/currentUser.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 const router = Router();
 
-router.route("/users/currentuser").get((_req, res, next) => {
-  console.log("req current user");
+router.route("/users/currentuser").get((req: Request, res: Response, next) => {
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+  }
 
   res.status(200).send("Req to currentuser OK");
 
