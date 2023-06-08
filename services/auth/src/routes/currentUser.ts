c9902049366530ae1975529e7ba11aba5af25ed2
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-06-07 20:30:09 -0400

    (9) auth- currentuser- add jwt validation logic

--- services/auth/src/routes/currentUser.ts
+++ services/auth/src/routes/currentUser.ts
@@ -1,13 +1,23 @@
 import { Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
 router.route("/users/currentuser").get((req: Request, res: Response, next) => {
-  if (!req.session?.jwt) {
+  const jwtSessionToken = req.session?.jwt;
+  if (jwtSessionToken) {
+    // handle error: check if jwt exists
     res.send({ currentUser: null });
-  }
+  } else {
+    // handle error: check if jwt is valid
+    try {
+      const payload = jwt.verify(jwtSessionToken, process.env.JWT_KEY!);
 
-  res.status(200).send("Req to currentuser OK");
+      res.status(200).send({ currentUser: payload });
+    } catch (err) {
+      res.send({ currentUser: null });
+    }
+  }
 
   next();
 });
