7961caaf6a834470bb5d3c9ac0517a808f01445d
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-07 15:16:23 -0500

    31-Add-login-tests (#32)

---
+++ services/auth/src/routes/__test__/defaults.ts
@@ -0,0 +1,14 @@
+export const email = {
+  valid: "test@test.org",
+  nonExistent: "missing@test.org",
+  invalid: "test test",
+};
+
+export const pw = {
+  valid: "password",
+  incorrect: "asdfasdf",
+  invalid: {
+    lenMin: "p",
+    lenMax: "a12345678901234567890",
+  },
+};
---
+++ services/auth/src/routes/__test__/login.test.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { app } from "../../app";
+// local
+import { email, pw } from "./defaults";
+/**
+ * Skipping validation tests. Will fail regardless because of validation for .
+ * Validation tests for login should be on frontend.
+ */
+it("Fails when email provided does not exist.", async () => {
+  await request(app)
+    .post("/api/users/login")
+    .send({ email: email.nonExistent, password: pw.valid })
+    .expect(400);
+});
+
+it("Fails when incorrect password is provided for valid account.", async () => {
+  // Create account
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: email.valid, password: pw.valid })
+    .expect(201);
+
+  // Attempt login with invalid password
+  await request(app)
+    .post("/api/users/login")
+    .send({ email: email.valid, password: pw.incorrect })
+    .expect(400);
+});
+
+it("Valid login returns cookie.", async () => {
+  // Create account
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: email.valid, password: pw.valid })
+    .expect(201);
+
+  // Attempt login with invalid password
+  const res: request.Response = await request(app)
+    .post("/api/users/login")
+    .send({ email: email.valid, password: pw.valid })
+    .expect(200);
+
+  expect(res.get("Set-Cookie")).toBeDefined();
+});
