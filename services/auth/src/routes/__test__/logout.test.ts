4247e28ac7d7557b0d1c9a6f8eb450198e0980bc
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-07 15:28:25 -0500

    (33) test: add logout test

---
+++ services/auth/src/routes/__test__/logout.test.ts
@@ -0,0 +1,29 @@
+import request from "supertest";
+import { app } from "../../app";
+// local
+import { email, pw } from "./defaults";
+
+it("Logout user successfully.", async () => {
+  // Create account
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: email.valid, password: pw.valid })
+    .expect(201);
+
+  // Attempt login with invalid password
+  const resLogin: request.Response = await request(app)
+    .post("/api/users/login")
+    .send({ email: email.valid, password: pw.valid })
+    .expect(200);
+
+  expect(resLogin.get("Set-Cookie")).toBeDefined();
+
+  const resLogout: request.Response = await request(app)
+    .post("/api/users/logout")
+    .send({})
+    .expect(200);
+
+  expect(resLogout.get("Set-Cookie")[0]).toEqual(
+    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+  );
+});
