846081c536e053893e336f09249671c51c08cdc1
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-07 20:09:12 -0500

    (39) feat: init nextjs (#40)

---
+++ client/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Outputs a Single-Page Application (SPA)
+  distDir: "./dist", // Changes the output directory `./dist/`
+};
+export default nextConfig;
---
+++ client/pages/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Main: FC = () => {
+  return <h1>Landing Page</h1>;
+};
+
+export default Main;
