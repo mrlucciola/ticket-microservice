305a16a2b0881c0ffbe30c5f85d5dd5359a02634
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-09 16:13:47 -0500

    (47) chore: update nextjs config to handle external files

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA)
-  distDir: "./dist", // Changes the output directory `./dist/`
+  webpackDevMiddleware: (config) => {
+    config.watchOptions.poll = 300;
+
+    return config;
+  },
+  experimental: {
+    externalDir: true,
+  },
 };
 export default nextConfig;
