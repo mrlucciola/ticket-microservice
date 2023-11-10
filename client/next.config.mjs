1acfc466c3cfee9fbb8ba57a8f146c185d219d7f
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-09 21:15:54 -0500

    (47) chore: update yarn script name and nextjs cfg

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions.poll = 300;
-
-    return config;
+  webpack: {
+    watchOptions: {
+      poll: 300,
+    },
   },
   experimental: {
     externalDir: true,
   },
+  output: "export", // Outputs a Single-Page Application (SPA)
+  distDir: "./dist", // Changes the output directory `./dist/`
 };
 export default nextConfig;
