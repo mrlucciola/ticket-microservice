61136d02e87047aa31a5b00e5c9f8e123a810afc
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2023-11-09 21:21:50 -0500

    47-Fix-deps-issue-in-yarn-workspace (#48)

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: {
+    watchOptions: {
+      poll: 300,
+    },
+  },
+  experimental: {
+    externalDir: true,
+  },
   output: "export", // Outputs a Single-Page Application (SPA)
   distDir: "./dist", // Changes the output directory `./dist/`
 };
